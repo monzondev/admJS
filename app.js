50eee2a6a54d4a05a139c913d9333f739c4989dc
monzondev <alexandermm2011@gmail.com>
2021-07-05 20:29:40 -0600

    cambio de libreria para la administracion de ldap

--- app.js
+++ app.js
@@ -1,42 +1,78 @@
-const {config} = require('dotenv');
 const express = require('express');
 const ldap = require('ldapjs');
 const CONFIG = require('./config.js')
-
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session')
 const session = require('express-session');
 const PassportLocal = require('passport-local').Strategy;
 const CustomStrategy = require('passport-custom').Strategy
-const {authenticate} = require('ldap-authentication');
 
 
+// INSTANCIA DE EXPRESS
 const app = express();
 
 // MOTOR DE VISTA
 app.set('view engine', 'ejs');
 
 
+// CREACION DE LA SESSION SEGUN RESULTADO DE AUTENTICACION
+passport.use('ldap', new CustomStrategy(async function (req, done) {
+    try {
+        if (! req.body.username || ! req.body.password) {
+            throw new Error('Usuario y contraseña no proporcionados')
+        }
+
+        // VALIDACION EN EL SERVIDOR LDAP
+        let username = "uid=" + req.body.username + "," + CONFIG.ldap.dn;
+        const user = await authenticateDN(username, req.body.password)
+        
+        // AUTENTICACION EXITOSA
+        done(null, user)
+    } catch (error) { 
+        // AUTENTICACION FALLIDA
+        done(null, null)
+
+    }
+}));
+
+// DETALLES DE LAS COOKIES EN SESION
+var sessionMiddleWare = cookieSession({
+    name: 'session',
+    keys: ['Manten el secreto solo para ti'],
+    maxAge: 60 * 60 * 1000 // 1 hora
+})
+
+// OPCIONES ESPECIALES PARA EL FUNCIONAMIENTO DE PASSPORT
+app.use(express.urlencoded({extended: true}));
+app.use(sessionMiddleWare);
+app.use(cookieParser('secretWords'));
+app.use(session({secret: 'secretWords', resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//PROCESO DE SERIALIZACION DEL RESULTADO DE LA AUTENTICACION
+passport.serializeUser(function (user, done) {
+    done(null, user);
+})
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+})
+
+
 // URL DEL SERVIDOR LDAP
 const client = ldap.createClient({url: CONFIG.ldap.url});
 
 // CREACION DE CONEXION A LDAP CON LOS PARAMETROS PASADOS POR LA VISTA LOGIN
-function authenticateDN(username, password) { /*bind use for authentication*/
-    client.bind(username, password, function (err) {
+function authenticateDN(username, password) { 
+  return client.bind(username, password, function (err) {
         if (err) {
             console.log("Error in new connetion " + err)
-        } else { /*if connection is success then go for any operation*/
-            console.log("Success");
-            // searchUser();
-            // addUser();
-            // deleteUser();
-            // addUserToGroup('cn=Administrators,ou=groups,ou=system');
-            // deleteUserFromGroup('cn=Administrators,ou=groups,ou=system');
-            // updateUser('cn=test,ou=users,ou=system');
-            // compare('cn=test,ou=users,ou=system');
-            //modifyDN('cn=bar,ou=users,ou=system');
-
+            return false;
+        } else { 
+            console.log(" Connection success");
+            return true;
         }
     });
 }
@@ -47,7 +83,6 @@ app.get("/adm", (req, res, next) => {
         return next();
      else 
         res.redirect("/adm/login")
-    
 }, (req, res) => {
     res.render("adm");
 });
@@ -58,16 +93,21 @@ app.get("/adm/login", (req, res, next) => {
         return next();
      else 
         res.redirect("/adm")
-    
 }, (req, res) => {
     res.render("login");
 });
 
-// EJECUCION DE LA FUNCION DE AUTENTICACION POSTERIOR AL POST DEL CLIENTE
-app.post("/adm/login", (req, res, next) => {
-    let user = "uid=" + req.body.username+ CONFIG.ldap.dn;
-    authenticateDN(user, req.body.password);
+//ACCION DE VERIFICACION DE LA AUTENTICACION Y REDIRECCION
+app.post("/adm/login", passport.authenticate('ldap', {
+    successRedirect: "/adm",
+    failureRedirect: "/adm/login"
+}));
+
+// CERRAR LA SESION DE LDAP SEGUN RESULTADO DE AUTENTICACION
+app.get("/adm/logout", function (req, res) {
+    req.logOut();
+    res.redirect("/adm/login");
 });
-let user = "uid=gauss, ou=example" ;
-authenticateDN(user, "password");
+
+//ASIGNACION DE PUERTO AL SERVIDOR
 app.listen(8002, () => console.log("Ya está corriendo, ya no lo toques más puerto 8002"));
--- config.js
+++ config.js
@@ -2,7 +2,9 @@ module.exports = {
     ldap: {
     //DATOS DEL USUARIO ADMINISTRADOR
       dn: 'dc=example,dc=com',
+      //dn: 'ou=admins,ou=sistemas,dc=enchiladas,dc=com'
     //URL DEL SERVIDOR LDAP
       url: 'ldap://ldap.forumsys.com'
+      //url: 'ldap://192.168.122.216:389' 
     }
 }
