cfd5b410251b6aa5a090d6912b6af75ae702ae75
monzondev <alexandermm2011@gmail.com>
2021-07-07 00:20:30 -0600

    Cambio de url y  dn para LDAP

--- app/config.js
+++ app/config.js
@@ -2,11 +2,13 @@ module.exports = {
     ldap: {
     //DATOS DEL USUARIO ADMINISTRADOR
       //dn: 'dc=example,dc=com',
-      dn: 'ou=admins,ou=sistemas,dc=enchiladas,dc=com',
+      //dn: 'ou=admins,ou=sistemas,dc=enchiladas,dc=com',
+      dn: 'dc=c,dc=crucial-study-312616,dc=internal',
       dn2: 'ou=enchiladas,ou=sistemas,dc=enchiladas,dc=com',
 
     //URL DEL SERVIDOR LDAP
       //url: 'ldap://ldap.forumsys.com'
-      url: 'ldap://192.168.255.30:389' 
+      //url: 'ldap://192.168.255.30:389' 
+      url: 'ldap://34.133.12.1:389'
     }
 }
